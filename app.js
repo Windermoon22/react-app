63a68a5d3b7a72cfc5ffa7ad607dd1e1d38484d6
ashwinimenaka <ashwini.meneka@gmail.com>
2025-05-27 16:12:23 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './src/navigation/AppNavigator';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+import ContactScreen from './screens/ContactScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <AppNavigator />
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="About" component={AboutScreen} />
+                <Stack.Screen name="Contact" component={ContactScreen} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
